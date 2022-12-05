43a15be7f381effe212ef2e108ee005d6870bafe
Trm888 <bigtram888@gmail.com>
2022-12-05 16:58:16 +0900

    First Commit

--- main.py
+++ main.py
@@ -1,5 +1,46 @@
+import collections
+import datetime
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 
+import pandas
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
+
+drinks_catalog = pandas.read_excel(
+    "wine3.xlsx",
+    sheet_name='Лист1',
+    usecols=['Категория', 'Название', 'Сорт', 'Цена', 'Картинка', 'Акция'],
+    na_values=['N/A', 'NA'],
+    keep_default_na=False
+).sort_values('Категория').to_dict(orient='records')
+
+formatted_drinks_catalog = collections.defaultdict(list)
+for drink in drinks_catalog:
+    formatted_drinks_catalog[drink['Категория']].append(drink)
+
+
+def get_age():
+    age = datetime.datetime.now().year - 1920
+    if age % 10 == 1 and age % 100 != 11:
+        return f'{age} год'
+    if age % 10 in [2, 3, 4] and not (age % 100 in [12, 13, 14]):
+        return f'{age} года'
+    return f'{age} лет'
+
+
+rendered_page = template.render(
+    age_text=get_age(),
+    drinks_catalog=formatted_drinks_catalog,
+)
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
